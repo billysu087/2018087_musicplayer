64fb6a16ece463b31fc201781aebbe5acc832886
Billianthony Sanendri Junior <billianthonysanendri@gmail.com>
2022-12-05 22:14:01 +0700

    2018087_musicplayer.git

---
+++ app/src/main/java/com/example/a2018087_musicplayer/AnotherPage.java
@@ -0,0 +1,16 @@
+package com.example.a2018087_musicplayer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.musicplayer.R;
+
+public class AnotherPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_page);
+    }
+}
